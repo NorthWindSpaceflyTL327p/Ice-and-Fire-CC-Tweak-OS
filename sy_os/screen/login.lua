b7af2c019fb631bf4c02b426306def2d6f4fc797
InnoZecroZ <inno.zecroz@gmail.com>
2025-12-17 01:28:51 +0700

    Add username password login

--- sy_os/screen/login.lua
+++ sy_os/screen/login.lua
@@ -5,7 +5,16 @@ function show()
 
     term.setCursorPos(2,3)
     print("Login Username: ")
+    term.setCursorPos(18,3)
+    local username = read()
 
     term.setCursorPos(2,4)
-    write("Password: ")
+    print("Password: ")
+    term.setCursorPos(12,4)
+    local password = read("*")
+
+    term.clear()
+    term.setCursorPos(2,2)
+    print("Logging in as " .. username .. "...")
+    sleep(1)
 end
