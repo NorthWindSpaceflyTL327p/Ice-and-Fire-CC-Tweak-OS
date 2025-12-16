7c6e00ed3183a812794f002f6ae7f6e0d91e25e3
InnoZecroZ <inno.zecroz@gmail.com>
2025-12-17 01:06:31 +0700

    Fix call API

--- sy_os/kernel.lua
+++ sy_os/kernel.lua
@@ -1,19 +1,24 @@
 -- Include necessary modules
 os.loadAPI("sy_os/lib/logg.lua")
 
--- Start the kernel
-logg.logg("Starting CC Tweak OS Kernel...")
--- (Kernel code would go here)
-logg.logg("CC Tweak OS Kernel started successfully.")
-
-
-
-
--- Load screen
+-- Load sy_os module
 os.loadAPI("sy_os/screen/splashscreen.lua")
-
-sy_os.screen.splashscreen.show()
-
 os.loadAPI("sy_os/screen/login.lua")
 
+-- Define sy_os table
+sy_os = {
+    screen = {
+        splashscreen = splashscreen,
+        login = login
+    }
+}
+
+-- Start the kernel
+logg.logg("Starting CC Tweak OS Kernel...")
+logg.logg("CC Tweak OS Kernel started successfully.")
+
+-- Show splashscreen
+sy_os.screen.splashscreen.show()
+
+-- Show login screen
 sy_os.screen.login.show()
